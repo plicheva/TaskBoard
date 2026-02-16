0d3a83facfae9ece6cfbd02364f2c10239b8016d
plicheva <p.licheva@gmail.com>
2026-02-16 14:23:37 +0200

    add files

---
+++ main.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b2817391-2d46-4f90-873a-aa8fc1883c84"
+}
+resource "random_integer" "ri" {
+  min = 10000
+  max = 50000
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+  location = var.resource_group_location
+}
+
+resource "azurerm_mssql_server" "sql" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+resource "azurerm_mssql_firewall_rule" "fwrule1" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sql.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name         = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+}
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+resource "azurerm_linux_web_app" "lwa" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};User ID=${azurerm_mssql_server.sql.administrator_login};Password=${azurerm_mssql_server.sql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+resource "azurerm_app_service_source_control" "github" {
+  app_id                 = azurerm_linux_web_app.lwa.id
+  repo_url               = var.URL_repo
+  branch                 = "main"
+  use_manual_integration = true
+}
+
+
